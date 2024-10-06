3e255aea638ddfcb43d42d2b2dc66d0c2aa6aad4
David Zvi Kadish <david.kadish@apiiro.com>
2024-10-06 17:46:59 +0300

    added function and function calls

--- test.js
+++ test.js
@@ -1,5 +1,13 @@
 import AWS from 'aws-sdk';
 
+function foo(x) {
+    console.log("In foo");
+    console.log("x =", x);
+}
+
 const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider();
 const STS_CLIENT = new AWS.STS();
 AWS.config.update({region: 'us-west-1'});
+
+x = parseInt();
+foo(x);
