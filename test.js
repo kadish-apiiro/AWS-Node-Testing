b65c1aa1172621d9a12e23a0ccba5bad45c3582d
David Zvi Kadish <david.kadish@apiiro.com>
2024-10-06 16:35:50 +0300

    added method call

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ import AWS from 'aws-sdk';
 
 const COGNITO_CLIENT = new AWS.CognitoIdentityServiceProvider();
 const STS_CLIENT = new AWS.STS();
+AWS.config.update({region: 'us-west-1', credentials: {YOUR_CREDENTIALS}});
